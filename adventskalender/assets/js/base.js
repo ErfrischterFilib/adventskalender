27efd22cbb552c502ccc702dd2de8f29e752751b
Philippe Weber <pw@erfrischung.ch>
2022-10-21 09:47:50 +0200

    Impl. Modal JS-Logik

--- adventskalender/assets/js/base.js
+++ adventskalender/assets/js/base.js
@@ -1,3 +1,46 @@
-function test(number) {
-    alert('Du hast' + number + 'geklickt!')
+let backgroundImage = document.getElementById('background');
+let iframe = document.getElementById('iframe');
+/*
+TODO: activate when live
+let today = new Date().getDate();
+*/
+
+let day = 2; // TODO: dynamic when live. see above
+
+console.log('Today is ' + day) //debug
+
+backgroundImage.addEventListener('load', function() {
+    let backgroundContent = backgroundImage.contentDocument;
+
+for (let i = 1; i <= day; i++){
+     let window = backgroundContent.getElementById(i);
+    window.addEventListener('click', function() {
+        openModal(i)
+    })
+}})
+// Get the modal
+let modal = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function openModal(window){
+    modal.style.display = "block";
+    iframe.contentDocument.getElementById('dayGif').src = "./../../images/gifs/" + window + ".gif"
+    iframe.contentDocument.getElementById('dayToday').innerHTML = window.toString()
+    iframe.contentDocument.getElementById('storyText').innerHTML = getText(window);
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
---
+++ adventskalender/assets/js/text.js
@@ -0,0 +1,6 @@
+function getText(day) {
+    switch (day) {
+        case 1: return "Es war<br>einmal<br>ein<br>line4"
+        case 2: return "Das<br>ist<br>Tag<br>2"
+
+    }}
