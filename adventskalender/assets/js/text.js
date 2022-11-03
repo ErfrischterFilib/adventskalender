5bb80218a0edfa6b5664975626a79af061825f2d
Philippe Weber <pw@erfrischung.ch>
2022-11-03 11:45:29 +0100

    Styling

--- adventskalender/assets/js/text.js
+++ adventskalender/assets/js/text.js
@@ -1,7 +1,7 @@
 function getText(day) {
     switch (day) {
         case 1:
-            return "Es war<br>einmal<br>ein<br>line4"
+            return "Emsige Freude im «Haus zum guten Glück»:<br>Vom Estrich herab nun Stück für Stück <br>Kerzen und Kugeln, Glitter und Stern<br>werden herbeigeschafft damit nah und fern<br>Weihnachten leuchtet auch dieses Jahr,<br>mit froher Botschaft und Fest als das schönste Paar"
         case 2:
             return "Das ist ein langer langer Text<br>an Tag 2 vom super duper<br>ultrakrassen geilomatico<br>Reka Familienhilfe Adventskalender<br>leider ist er noch biz hässlich<br>gestylt"
 
