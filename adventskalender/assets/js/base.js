243dd01dd80c7b729829a3f046ae7d933e7e03f8
Philippe Weber <pw@erfrischung.ch>
2022-11-10 13:14:57 +0100

    pfeile in modal

--- adventskalender/assets/js/base.js
+++ adventskalender/assets/js/base.js
@@ -9,15 +9,16 @@ let day = 2; // TODO: dynamic when live. see above
 
 console.log('Today is ' + day) //debug
 
-backgroundImage.addEventListener('load', function() {
+backgroundImage.addEventListener('load', function () {
     let backgroundContent = backgroundImage.contentDocument;
 
-for (let i = 1; i <= day; i++){
-     let window = backgroundContent.getElementById(i);
-    window.addEventListener('click', function() {
-        openModal(i)
-    })
-}})
+    for (let i = 1; i <= day; i++) {
+        let window = backgroundContent.getElementById(i);
+        window.addEventListener('click', function () {
+            openModal(i)
+        })
+    }
+})
 // Get the modal
 let modal = document.getElementById("modal");
 
@@ -25,20 +26,19 @@ let modal = document.getElementById("modal");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-function openModal(window){
+function openModal(day) {
     modal.style.display = "block";
-    iframe.contentDocument.getElementById('dayGif').src = "./../../images/gifs/" + window + ".gif"
-    iframe.contentDocument.getElementById('dayToday').innerHTML = window.toString()
-    iframe.contentDocument.getElementById('storyText').innerHTML = getText(window);
+    iframe.contentDocument.getElementById('dayGif').src = "/assets/images/gifs/" + day + ".gif"
+    iframe.contentDocument.getElementById('dayToday').innerHTML = day.toString()
+    iframe.contentDocument.getElementById('storyText').innerHTML = getText(day);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+
+span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
