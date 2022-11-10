4c106118298dba31cb649572f17d310488802a90
Philippe Weber <pw@erfrischung.ch>
2022-11-10 16:40:31 +0100

    alle deutschen texte

--- adventskalender/assets/js/base.js
+++ adventskalender/assets/js/base.js
@@ -2,17 +2,17 @@ let backgroundImage = document.getElementById('background');
 let iframe = document.getElementById('iframe');
 /*
 TODO: activate when live
-let today = new Date().getDate();
+let day = new Date().getDate();
 */
 
-let day = 2; // TODO: dynamic when live. see above
+let today = 24; // TODO: dynamic when live. see above
 
-console.log('Today is ' + day) //debug
+console.log('Today is ' + today) //debug
 
 backgroundImage.addEventListener('load', function () {
     let backgroundContent = backgroundImage.contentDocument;
 
-    for (let i = 1; i <= day; i++) {
+    for (let i = 1; i <= today; i++) {
         let window = backgroundContent.getElementById(i);
         window.addEventListener('click', function () {
             openModal(i)
@@ -27,10 +27,32 @@ let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 function openModal(day) {
+    let arrowLeft = iframe.contentDocument.getElementById("arrowLeft");
+    let arrowRight = iframe.contentDocument.getElementById("arrowRight");
     modal.style.display = "block";
     iframe.contentDocument.getElementById('dayGif').src = "/assets/images/gifs/" + day + ".gif"
     iframe.contentDocument.getElementById('dayToday').innerHTML = day.toString()
     iframe.contentDocument.getElementById('storyText').innerHTML = getText(day);
+
+    if (day > 1) {
+        arrowLeft.style.display = "";
+        arrowLeft.addEventListener("click", function () {
+                openModal(day - 1)
+            }
+        )
+    } else {
+        arrowLeft.style.display = "none";
+    }
+
+    if (day < today) {
+        arrowRight.style.display = "";
+        iframe.contentDocument.getElementById('arrowRight').addEventListener("click", function () {
+                openModal(day + 1)
+            }
+        )
+    } else {
+        arrowRight.style.display = "none";
+    }
 }
 
 
--- adventskalender/assets/js/text.js
+++ adventskalender/assets/js/text.js
@@ -1,9 +1,52 @@
 function getText(day) {
     switch (day) {
         case 1:
-            return "Emsige Freude im «Haus zum guten Glück»:<br>Vom Estrich herab nun Stück für Stück <br>Kerzen und Kugeln, Glitter und Stern<br>werden herbeigeschafft damit nah und fern<br>Weihnachten leuchtet auch dieses Jahr,<br>mit froher Botschaft und Fest als das schönste Paar"
+            return "Trubel im &laquo;Haus zum guten Gl&uuml;ck&raquo;:<br />Vom Estrich herab nun St&uuml;ck f&uuml;r St&uuml;ck<br />Kerzen und Kugeln, Glitter und Stern<br />sollen herbei, weil alle so gern<br />den Zauber erleben und festlich werden.<br />Auf frohe Botschaften und Liebe auf Erden!"
         case 2:
-            return "Das ist ein langer langer Text<br>an Tag 2 vom super duper<br>ultrakrassen geilomatico<br>Reka Familienhilfe Adventskalender<br>leider ist er noch biz hässlich<br>gestylt"
-
+            return "Doch ach, vermisst wird das geliebte K&auml;tzchen!<br />Wer kann wachen und schlafen ohne das Sch&auml;tzchen?<br />Ans Feiern ist nun nicht mehr zu denken,<br />nur Miezchen k&ouml;nnte Trost jetzt schenken&nbsp;..."
+        case 3:
+            return "&hellip;doch gross ist die Freude! Denn da, ganz weit oben,<br />schl&auml;ft Maunzelchen wie auf dem Ofen.<br />Welch&rsquo; Stein f&auml;llt vom Herzen! Und auf dem Kamin <br />tanzt auch die Maus, voll Schwung in den Knien."
+        case 4:
+            return "Strasse, Flush, Full House mit Ass:<br />Wer blufft, bleibt cool, sonst kostet&rsquo;s was.<br />Beim Poker gilt: Nicht lange fackeln! <br />Da l&auml;sst ein Beben die Lampe wackeln &hellip;"
+        case 5:
+            return "&hellip;kein Beben, doch steigt ein K&auml;ferfest:<br />&laquo;Sturmfrei! Egal, dann gibt&rsquo;s halt Hausarrest!&raquo;,<br />das rechnen die Kinder, die keiner kann stoppen,<br />so ein H&uuml;pfburg-Spass ist auch nicht zu toppen."
+        case 6:
+            return "Auf dem Velo legt zu einen Zahn<br />der Samichlaus im Fitnesswahn.<br />&laquo;Wie soll denn sonst am Tag der Tage<br />mein Auftritt gl&uuml;cken?&raquo;, ist seine Frage.<br />Doch Zweifel weckt das Bild bei dem, der fragt:<br />W&auml;r nicht grad heut sein Auftritt angesagt?"
+        case 7:
+            return "Zwei rechts, zwei links und blitzeschnell,<br />das wird kein Pulli f&uuml;rs Fidos Fell &ndash; <br />so gern er ihn h&auml;tt. Doch Chlaus braucht die M&uuml;tze,<br />drum schwirren die Nadeln, auf dass es ihm n&uuml;tze."
+        case 8:
+            return "Was w&auml;r das Christfest ohne Musik?<br />Das &laquo;gute Gl&uuml;ck&raquo; gibt die laute Replik.<br />Man trifft sich gerade zum gemeinsamen Proben,<br />die Musiker sich feierliche Stimmung geloben.<br />Auch wenn alles begann mit &laquo;Stille Nacht&raquo;,<br />es hat eine rockige Party entfacht &hellip;"
+        case 9:
+            return "&hellip;das findet zumindest die schlaflose Seele.<br />Ihr ist, als ob der Sound ihr die Nacht ganz stehle:<br />&laquo;Geklimper, Gequietsche!&raquo;, sie &auml;rgert sich blau.<br />Doch friedlich schlummern die Katz und die Frau."
+        case 10:
+            return "Noch vierzehn Tage! H&ouml;chste Zeit,<br />unterm Dach zu vollenden, was l&auml;ngst h&auml;ngt bereit.<br />Denn Weihnachten im &laquo;guten Gl&uuml;ck&raquo;<br />soll bunt sein und gl&auml;nzen. Drum: &laquo;Schm&uuml;ck <br />dich f&uuml;rs Fest, putz dich heraus!&raquo;<br />So wollen es alle vom kunterbunten Haus."
+        case 11:
+            return "Das Fest der Liebe, man erz&auml;hlt&rsquo;s immer wieder,<br />bringt Gl&uuml;hwein und Braten, Geschenke und Lieder.<br />Und heute wagt&rsquo;s einer und &ouml;ffnet sein Herz!<br />Leicht ist das nicht, denn er meint&rsquo;s nicht im Scherz. <br />Ein Riesenstrauss Rosen dient als Versteck ihm ganz gut,<br />hinter duftender Pracht sammelt er all seinen Mut."
+        case 12:
+            return "Nicht nur die Frisur Hund und Frauchen verbindet,<br />ein Herz fand zum andern, sodass die Liebe nie schwindet.<br />Gerade wieder beim TV-Spass sieht man: Wie wild<br />freut sich Pudel &uuml;bers vermeintliche Spiegelbild."
+        case 13:
+            return "&laquo;Nicht auszuhalten, diese Hitze!&raquo;<br />Dem Schneemann vergehn nicht nur die Witze.<br />Er selbst schmilzt gar dahin. Was soll nun werden?<br />Das w&auml;r ja dramatisch, so ein Ende auf Erden!<br />Die Koffer zum Nordpol sind drum stets bereit, <br />zur Not &ouml;ffnet er erst mal den Frigo ganz weit."
+        case 14:
+            return "Die K&uuml;nstlerin greift in die Vollen,<br />zu leuchtenden Farben und ganz grossen Rollen.<br />Mit schwungvoller Geste, doch sicherer Hand,<br />malt sie Weihnachten direkt an die Wand!"
+        case 15:
+            return "Was gibt es Sch&ouml;n&rsquo;res f&uuml;r Mutter und Kind,<br />wenn Zimtstern und Brunsli angesagt sind?<br />Doch die Guetsli, am Fenster zum K&uuml;hlen,<br />kippen und fallen! Mit aufgew&uuml;hlten Gef&uuml;hlen<br />rufen die B&auml;ckerinnen, klein und gross,<br />der zuckrigen Fracht nach: &laquo;Was machen wir bloss?&raquo;"
+        case 16:
+            return ">&laquo;Wir fangen sie auf!&raquo;, ruft es von unten ganz helle.<br />Zum Gl&uuml;ck ist die WG im F&uuml;nften zur Stelle. <br />Bei ihr landet ganz sicher das Fluggeb&auml;ck:<br />als Guetsliregen und feiner Snack."
+        case 17:
+            return "Vor der and&auml;chtigen Stille, den Kerzen am Baum,<br />die Nacht durchtanzen &ndash; bis zum Morgengraun!<br />Das Bistro hat die Tische zur Seite geschoben, <br />jetzt drehen sich Paare, und sie erproben<br />Standards und lieber noch freie Figuren.<br />Auf Schwung und Hingabe, so stehen die Uhren."
+        case 18:
+            return "Aufrichte? Das Haus steht doch schon lang.<br />O Tannenbaum doch, bekannt als Gesang, <br />soll es nun festlich schm&uuml;cken. Drum ruft man stolz:<br />&laquo;Mit vereinter Kraft &ndash; hoch mit dem Nadelholz!&raquo;"
+        case 19:
+            return "Rasiert, dreit&auml;gig, in voller F&uuml;lle der Bart?<br />Wuchtig die Krone oder eher ganz zart?<br />Der Mantel weit wehend oder doch vielmehr eng?<br />Milde als Herrscher oder lieber gestreng?<br />Wer ist der sch&ouml;nste K&ouml;nig im Spiegelschein? <br />Wie weise und nach dem letzten Schrei sein?"
+        case 20:
+            return "Der P&ouml;stler spielt eine tragende Rolle. <br />Sein Schlitten, mit Geschenken volle,<br />h&auml;lt auch beim &laquo;guten Gl&uuml;ck&raquo;. Was versteckt<br />buntes Papier, was hat da jemand ausgeheckt?"
+        case 21:
+            return "&hellip;der Skifan &uuml;bt schon fleissig-trocken,<br />Knie tief gebeugt, f&uuml;r sch&ouml;ne Hocken.<br />Um &uuml;ber Berge zu brettern, braucht&rsquo;s nur noch eins &ndash; <br />da klingelt&rsquo;s! Ist das grosse Pack wohl seins?"
+        case 22:
+            return "Kreuz und quer ziehn Fonduef&auml;den. Strafe setzt&rsquo;s<br />f&uuml;r den, der seinen Mocken fallenl&auml;sst<br />in den K&auml;s: Ein Lied bei verlor&rsquo;nem Bissen<br />singen, das bl&uuml;ht jedem. Man muss auch wissen: <br />So will&rsquo;s die Tradition im &laquo;guten Gl&uuml;ck&raquo;,<br />gesungen wird immer f&uuml;r jedes St&uuml;ck."
+        case 23:
+            return "Darauf hat die ganze Welt sich gefreut, <br />alle Sorgen und Eile verfliegen heut. <br />&laquo;Vom Himmel hoch, da komm ich her!&raquo;, <br />seilt sich Christkind ab und schmunzelt sehr.<br />Jetzt kommt es an, ein Stern am Haupt,<br />im &laquo;guten Gl&uuml;ck&raquo;, wo jeder an die Botschaft glaubt &hellip;"
+        case 24:
+            return "&hellip; dass f&uuml;reinander da sein, Liebe und Freude<br />die st&auml;rksten M&auml;chte sind. In dem Geb&auml;ude <br />mit &laquo;gutem Gl&uuml;ck&raquo; im Namen wie in der weiten Welt:<br />Friede soll herrschen unter dem Himmelszelt. <br />F&uuml;r dich, f&uuml;r euch, f&uuml;r Sie alle, ganz klar: <br />Frohe Weihnachten und ein gutes neues Jahr!"
     }
 }
