d08b54c61c51835e23a67dece061ae3bee619bd9
Philippe Weber <pw@erfrischung.ch>
2022-11-25 11:48:26 +0100

    fertig ausser live-tageslogik

--- adventskalender/assets/js/base.js
+++ adventskalender/assets/js/base.js
@@ -1,11 +1,10 @@
 let backgroundImage = document.getElementById('background');
-let iframe = document.getElementById('iframe');
 /*
 TODO: activate when live
 let day = new Date().getDate();
 */
 
-let today = 8; // TODO: dynamic when live. see above
+let today = 11; // TODO: dynamic when live. see above
 
 console.log('Today is ' + today) //debug
 
@@ -27,12 +26,12 @@ let modal = document.getElementById("modal");
 let span = document.getElementsByClassName("close")[0];
 
 function openModal(day) {
-    let arrowLeft = iframe.contentDocument.getElementById("arrowLeft");
-    let arrowRight = iframe.contentDocument.getElementById("arrowRight");
+    let arrowLeft = document.getElementById("arrowLeft");
+    let arrowRight = document.getElementById("arrowRight");
     modal.style.display = "block";
-    iframe.contentDocument.getElementById('dayGif').src = "/assets/images/gifs/" + day + ".gif"
-    iframe.contentDocument.getElementById('dayToday').innerHTML = day.toString()
-    iframe.contentDocument.getElementById('storyText').innerHTML = getText(day);
+    document.getElementById('dayGif').src = "/assets/images/gifs/" + day + ".gif"
+    document.getElementById('dayToday').innerHTML = day.toString()
+    document.getElementById('storyText').innerHTML = getText(day);
 
     if (day > 1) {
         arrowLeft.style.display = "";
@@ -46,7 +45,7 @@ function openModal(day) {
 
     if (day < today && day < 24) {
         arrowRight.style.display = "";
-        iframe.contentDocument.getElementById('arrowRight').addEventListener("click", function () {
+        document.getElementById('arrowRight').addEventListener("click", function () {
                 openModal(day + 1)
             }
         )
@@ -55,14 +54,11 @@ function openModal(day) {
     }
 }
 
-
 span.onclick = function () {
     modal.style.display = "none";
 }
-
 window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
 }
-
---
+++ adventskalender/assets/js/text_fr.js
@@ -0,0 +1,52 @@
+function getText(day) {
+    switch (day) {
+        case 1:
+            return "Quelle agitation dans le salon !<br />Rubans et couronnes pendent du plafond<br />D&eacute;cor&eacute;s d&rsquo;&eacute;toiles et de bougies<br />Qui partout de mille feux scintillent<br />Pour accueillir de belle mani&egrave;re<br />Le grand message d&rsquo;amour sur Terre."
+        case 2:
+            return "O&ugrave; donc s&rsquo;est perdu le petit chat<br />Que la fillette affiche par l&agrave; ?<br />Impossible de penser &agrave; f&ecirc;ter<br />Avant de l&rsquo;avoir retrouv&eacute; !"
+        case 3:
+            return "Mais regarde qui dort l&agrave;-haut<br />Sur la chemin&eacute;e bien au chaud.<br />C&rsquo;est la petite boule de poils tant aim&eacute;e<br />Enfin, minou a &eacute;t&eacute; retrouv&eacute; !"
+        case 4:
+            return "Suite, carr&eacute; ou brelan d&rsquo;as aux rois<br />Celui qui bluffe ne le montre pas.<br />Au poker il faut &ecirc;tre s&ucirc;r de soi<br />Mais qu&rsquo;est-ce qui arrive &agrave; cette lampe-l&agrave; ?"
+        case 5:
+            return "Ce n&rsquo;est pas un tremblement de terre<br />Antoine, Pierre et Julie sautent sur le lit<br />Alors que les parents sont partis<br />Ils ont d&eacute;marr&eacute; une f&ecirc;te d&rsquo;enfer !"
+        case 6:
+            return "Sur son v&eacute;lo perch&eacute;, saint Nicolas<br />Fait son entra&icirc;nement ce jour-l&agrave;.<br />Il faudra qu&rsquo;il soit en pleine forme<br />Pour conduire son tra&icirc;neau &eacute;norme."
+        case 7:
+            return "Deux mailles &agrave; gauche et deux &agrave; droite<br />Grand-m&egrave;re est vraiment tr&egrave;s adroite<br />Mais le beau bonnet qu&rsquo;elle tricote<br />N&rsquo;est pas pour son chien Aristote<br />Avec son grand pompon tout blanc<br />Il ira bien mieux &agrave; Vincent."
+        case 8:
+            return "Que serait un No&euml;l sans musique ?<br />Tout le monde a l&rsquo;esprit lyrique<br />Et joue ou chante pour mettre de l&rsquo;ambiance<br />Et pourquoi pas quelques pas de danse."
+        case 9:
+            return "Tout ce bruit, le voisin ne l&rsquo;appr&eacute;cie gu&egrave;re<br />Et esp&egrave;re qu&rsquo;ils veuillent bien se taire.<br />Sa femme et son chat font bien mieux :<br />Ils dorment comme des bienheureux."
+        case 10:
+            return "Plus que deux semaines, il est grand temps<br />De mettre en place tous les rubans<br />Pour que No&euml;l de mille feux brille <br />Dans cette maison et toute la ville.<br />Elle met &eacute;toiles et boules en verre<br />D&rsquo;un bout &agrave; l&rsquo;autre de la barri&egrave;re."
+        case 11:
+            return "La f&ecirc;te de l&rsquo;amour, on le sait bien<br />Rime avec r&eacute;veillon et bon vin<br />Mais ce soir, cach&eacute; derri&egrave;re ses fleurs<br />Un grand timide lui ouvre son c&oelig;ur <br />A la main un bouquet de roses<br />Enfin lui dire sa flamme il ose."
+        case 12:
+            return "Ils partagent coiffure et salon<br />Et regardent sur leur futon<br />D&rsquo;un autre caniche les aventures<br />Qui leur rappellent celles qu&rsquo;ils eurent."
+        case 13:
+            return "Insupportable cette chaleur<br />Le pauvre souffre depuis des heures<br />Que peut faire un bonhomme de neige<br />Dans un beau salon pris au pi&egrave;ge ?<br />Il ouvre grand le r&eacute;frig&eacute;rateur<br />Mais fond quand m&ecirc;me comme du beurre."
+        case 14:
+            return "Dans un grand et deux petits pots<br />La jeune artiste plonge son rouleau<br />Pour reproduire sur le mur du fond<br />Le roi des for&ecirc;ts pour de bon."
+        case 15:
+            return "Ici on cuisine des biscuits<br />Et des croissants &agrave; la vanille<br />Et d&egrave;s qu&rsquo;ils sont sortis du four<br />Sur la fen&ecirc;tre ils vont faire un tour.<br />Mais attention, petite fille<br />V&rsquo;la la moiti&eacute; qui part en vrille !"
+        case 16:
+            return "Mais les voisins tendent les bras<br />&laquo; Nous les avons ! &raquo;, crie-t-on d&rsquo;en bas<br />Les biscuits ne sont pas perdus<br />Et au jeune gar&ccedil;on ils ont bien plu."
+        case 17:
+            return "Avant d&rsquo;aller se recueillir<br />Tous les clients dansent avec plaisir.<br />Les tables sont pouss&eacute;es de c&ocirc;t&eacute;<br />Et les couples parfois enlac&eacute;s<br />Ou dans des figures modernes<br />Dansent jusqu&rsquo;&agrave; en perdre haleine"
+        case 18:
+            return "Que vient faire un sapin sur le toit ?<br />Il n&rsquo;est pas &agrave; sa place celui-l&agrave; !<br />Ne serait-il pas mieux au salon<br />Berc&eacute; d&rsquo;une multitude de chansons ?"
+        case 19:
+            return "Les rois mages choisissent leur couronne<br />Pour cela pas besoin de tr&ocirc;ne <br />Un miroir sera suffisant<br />Pour bien s&rsquo;assurer du seyant<br />Ils ont d&eacute;j&agrave; mis leur manteau<br />Et vont s&ucirc;rement partir bient&ocirc;t."
+        case 20:
+            return "Le facteur ne ch&ocirc;me pas non plus<br />Tant de cadeaux sont attendus !<br />Que cache donc le grand paquet vert ?<br />Tu ne l&rsquo;sauras qu&rsquo;une fois ouvert !"
+        case 21:
+            return "Oh mais le skieur est impatient <br />De mettre &agrave; l&rsquo;&eacute;preuve son talent<br />Il s&rsquo;entra&icirc;ne d&eacute;j&agrave;, persuad&eacute;<br />Que ses skis ne vont plus tarder."
+        case 22:
+            return "Tous les quatre autour du caquelon<br />Trouvent que la fondue c&rsquo;est bien bon<br />Et si les fils s&rsquo;emm&ecirc;lent parfois<br />Rien de grave &agrave; tout &ccedil;a<br />Mais si l&rsquo;un d&rsquo;eux y perd son pain<br />Dans la fontaine va prendre un bain !"
+        case 23:
+            return "A la plus haute chemin&eacute;e<br />Un ange coquin s&rsquo;est attach&eacute;<br />Pour, gr&acirc;ce &agrave; ses grandes ailes,<br />Nous apporter la bonne nouvelle."
+        case 24:
+            return "L&rsquo;amour, le partage et la joie<br />Sont plus forts que les plus grands rois.<br />Ensemble, quelle que soit notre maison<br />Cherchons la paix &agrave; l&rsquo;unisson.<br />Joyeux No&euml;l sur toute la Terre !<br />Pour la grande la f&ecirc;te de la lumi&egrave;re<br />Et que l&rsquo;ann&eacute;e qui pointe son nez<br />Vous accorde bonheur et sant&eacute; !"
+    }
+}
